6454da4df51e98d4aa8470d88441dfb209db6dd4
Aesthetic <andreiclima5@gmail.com>
2024-02-17 16:38:40 +0200

    Email func update Password encrypt Storage test

--- src/Storage.java
+++ src/Storage.java
@@ -1,4 +1,14 @@
+import java.util.TreeMap;
+
 public class Storage {
     private Create_Account Acc;
+    private TreeMap<String, Create_Account> AccountInfo = new TreeMap<>();
+    public void addAccount(Create_Account Acc, String username){
+        AccountInfo.put(username, Acc);
+    }
+
+    public void toString(String username){
+        System.out.println(AccountInfo.get(username));
+    }
 
 }
